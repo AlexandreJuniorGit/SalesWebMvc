baafbef342c153d3517096a49ee6ab5a5d993e46
Alexandre Aparecido <aadsj.alexandre@gmail.com>
2022-04-19 20:12:52 -0300

    Returning custom error page

--- SalesWebMvc/Models/ViewModels/ErrorViewModel.cs
+++ SalesWebMvc/Models/ViewModels/ErrorViewModel.cs
@@ -5,6 +5,7 @@ namespace SalesWebMvc.Models.ViewMoldels
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
+        public string Message { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
